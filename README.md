087450933200767350b5da5be74a701c86ddba96
Ryan Brewster <ryan.brewster@akqa.com>
2013-03-22 12:42:59 -0700

    added readme

--- README.md
+++ README.md
@@ -1,2 +1,30 @@
-task-generator
+Task Generator
 ==============
+
+* Install Ruby!
+* Run `ruby task-generator.rb`
+* Notify Management
+* Return To Work
+
+Sample Output
+=============
+```script
+> ruby task_generator.rb
+
+How many useless tasks do you need?
+10
+
+-----=====-----
+Conform Compatible Assets
+Conform Fragile Software
+Test Regressive Development
+Architect Object-oriented Component
+Conform Corrective Threshold
+Develop Testable Artifacts
+Survey Inconsistent Interface
+Measure Dynamic Artifacts
+Engineer Traceable Component
+Transform Traceable Work
+-----=====-----
+
+```
--- task_generator.rb
+++ task_generator.rb
@@ -1,3 +1,4 @@
+puts
 puts "How many useless tasks do you need?"
 amount = Integer(gets) rescue 1
 
@@ -5,6 +6,10 @@ VERBS = %w{abstract adapt analyze architect clone compress conform connect corre
 ADJECTIVES = %w{adaptive agile alternative client compatible complex corrective dynamic effeciency experimental fragile inconsistent object-oriented preliminary regressive reliable reusable robust specialized statistical testable traceable}
 NOUNS = %w{analysis artifacts assets benchmarks component connectors designs development effort features interface latency maintenance model paradigm pattern scope software syntax tests threshold version work}
 
+puts
+puts '-----=====-----'
 amount.times do
   puts [VERBS.sample.capitalize, ADJECTIVES.sample.capitalize, NOUNS.sample.capitalize] * ' '
 end
+puts '-----=====-----'
+puts
